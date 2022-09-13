155a30d4ff6b6b6a57d85ede5a29a4f13f2742be
邵天扬 <codeinpeace@163.com>
2022-09-13 11:32:59 +0800

    资产模块钉钉端controller类

---
+++ it-asset/src/main/java/com/hexing/asset/controller/DingTalkAssetController.java
@@ -0,0 +1,12 @@
+package com.hexing.asset.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 开放给钉钉端的rest接口
+ */
+@RestController
+@RequestMapping("/api/dingtalk/asset")
+public class DingTalkAssetController {
+}

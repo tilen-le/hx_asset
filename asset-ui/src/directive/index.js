426d9516e91f92813170fb0c3632a3870ef8f264
80009313 <@80009313>
2022-09-14 10:49:55 +0800

    资产列表-保管人工号字段变更 全局添加显隐列功能

--- asset-ui/src/directive/index.js
+++ asset-ui/src/directive/index.js
@@ -1,21 +1,23 @@
-import hasRole from './permission/hasRole'
-import hasPermi from './permission/hasPermi'
-import dialogDrag from './dialog/drag'
-import dialogDragWidth from './dialog/dragWidth'
-import dialogDragHeight from './dialog/dragHeight'
+import hasRole from "./permission/hasRole";
+import hasPermi from "./permission/hasPermi";
+import dialogDrag from "./dialog/drag";
+import dialogDragWidth from "./dialog/dragWidth";
+import dialogDragHeight from "./dialog/dragHeight";
+import columns from "./table/columns";
 
-const install = function(Vue) {
-  Vue.directive('hasRole', hasRole)
-  Vue.directive('hasPermi', hasPermi)
-  Vue.directive('dialogDrag', dialogDrag)
-  Vue.directive('dialogDragWidth', dialogDragWidth)
-  Vue.directive('dialogDragHeight', dialogDragHeight)
-}
+const install = function (Vue) {
+  Vue.directive("hasRole", hasRole);
+  Vue.directive("hasPermi", hasPermi);
+  Vue.directive("dialogDrag", dialogDrag);
+  Vue.directive("dialogDragWidth", dialogDragWidth);
+  Vue.directive("dialogDragHeight", dialogDragHeight);
+  Vue.directive("columns", columns);
+};
 
 if (window.Vue) {
-  window['hasRole'] = hasRole
-  window['hasPermi'] = hasPermi
+  window["hasRole"] = hasRole;
+  window["hasPermi"] = hasPermi;
   Vue.use(install); // eslint-disable-line
 }
 
-export default install
+export default install;

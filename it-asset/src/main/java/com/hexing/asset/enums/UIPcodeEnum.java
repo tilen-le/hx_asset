82862d7128c0962ee17b42fd4f3c7c40142b56b6
邵天扬 <codeinpeace@163.com>
2022-09-15 18:19:31 +0800

    平台资产编号同步SAP

---
+++ it-asset/src/main/java/com/hexing/asset/enums/UIPcodeEnum.java
@@ -0,0 +1,28 @@
+package com.hexing.asset.enums;
+
+public enum UIPcodeEnum {
+
+
+    pushAssetInfoToSAP("ZFMFICOZC002", "将资产信息与保管人关联关系同步到SAP"),
+    pushSingleFdCodeToSAP("ZFMFICOZC003", "将通过Excel导入的新资产数据的平台资产编码同步到SAP"),
+    ;
+
+
+    private String code;
+    private String msg;
+
+    UIPcodeEnum(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+
+}

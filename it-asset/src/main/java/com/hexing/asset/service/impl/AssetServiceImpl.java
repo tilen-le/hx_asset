75fde9982484eeb1812d8e4ca25f5d95671090ca
邵天扬 <codeinpeace@163.com>
2022-09-15 14:23:38 +0800

    资产列表bug修复

--- it-asset/src/main/java/com/hexing/asset/service/impl/AssetServiceImpl.java
+++ it-asset/src/main/java/com/hexing/asset/service/impl/AssetServiceImpl.java
@@ -69,7 +69,7 @@ public class AssetServiceImpl extends ServiceImpl<AssetMapper, Asset> implements
     public Asset selectAssetByAssetCode(String assetCode) {
         QueryWrapper<Asset> wrapper = new QueryWrapper<>();
         wrapper.eq("asset_code", assetCode);
-        Asset asset = new Asset();
+        Asset asset = assetMapper.selectOne(wrapper);
         Map<String, SysUser> usernameNicknameMap = sysUserService.getUsernameUserObjMap();
         Map<String, String> deptIdDeptNameMap = sysDeptService.getDeptIdDeptNameMap();
 
@@ -260,7 +260,7 @@ public class AssetServiceImpl extends ServiceImpl<AssetMapper, Asset> implements
         if (CollectionUtil.isNotEmpty(assetList)) {
             for (Asset asset : assetList) {
                 SysUser user = usernameNicknameMap.get(asset.getResponsiblePersonCode());
-                asset.setResponsiblePersonName(user.getUserName());
+                asset.setResponsiblePersonName(user.getNickName());
                 asset.setResponsiblePersonDept(deptIdDeptNameMap.get(user.getDeptId().toString()));
             }
         }

c7d3a64acebcdb3329885aa563b9677ae8f78053
80010712 <15122505230@163.com>
2022-09-15 14:42:13 +0800

    盘点任务

--- it-asset/src/main/java/com/hexing/asset/service/impl/AssetInventoryTaskServiceImpl.java
+++ it-asset/src/main/java/com/hexing/asset/service/impl/AssetInventoryTaskServiceImpl.java
@@ -1,23 +1,34 @@
 package com.hexing.asset.service.impl;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hexing.asset.domain.Asset;
-import com.hexing.asset.domain.AssetInventoryTask;
+import com.hexing.asset.domain.AssetProcess;
 import com.hexing.asset.domain.AssetProcessCounting;
+import com.hexing.asset.domain.AssetInventoryTask;
 import com.hexing.asset.domain.dto.AssetInventoryTaskDTO;
 import com.hexing.asset.enums.AssetCountingStatus;
 import com.hexing.asset.mapper.AssetInventoryTaskMapper;
 import com.hexing.asset.mapper.AssetMapper;
-import com.hexing.asset.service.IAssetInventoryTaskService;
+import com.hexing.asset.mapper.AssetProcessMapper;
 import com.hexing.asset.service.IAssetProcessCountingService;
+import com.hexing.asset.service.IAssetInventoryTaskService;
+import com.hexing.asset.service.IAssetProcessService;
+import com.hexing.common.core.domain.entity.SysDept;
+import com.hexing.common.core.domain.entity.SysUser;
+import com.hexing.common.utils.DateUtils;
+import com.hexing.common.utils.SecurityUtils;
+import com.hexing.system.service.impl.SysDeptServiceImpl;
+import com.hexing.system.service.impl.SysUserServiceImpl;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
-import java.util.Date;
-import java.util.List;
 
 /**
  * 盘点任务Service业务层处理
@@ -35,10 +46,17 @@ public class AssetInventoryTaskServiceImpl extends ServiceImpl<AssetInventoryTas
     private AssetInventoryTaskMapper assetInventoryTaskMapper;
     @Autowired
     private AssetMapper assetMapper;
+    @Autowired
+    private AssetProcessMapper assetProcessMapper;
 
     @Autowired
     private IAssetProcessCountingService assetProcessCountingService;
-
+    @Autowired
+    private IAssetProcessService assetProcessService;
+    @Autowired
+    private SysUserServiceImpl sysUserService;
+    @Autowired
+    private SysDeptServiceImpl sysDeptService;
     /**
      * 查询盘点任务
      *
@@ -72,25 +90,49 @@ public class AssetInventoryTaskServiceImpl extends ServiceImpl<AssetInventoryTas
     {
         AssetInventoryTask task = new AssetInventoryTask();
         BeanUtils.copyProperties(dto, task);
-
-        // 统计待盘点资产总数
-        task.setAssetCounted(0);   /* 已盘点资产数 */
-        task.setAssetAbnormal(0);  /* 异常资产数目 */
-        task.setAssetNotCounted(0); /* 待盘点资产数 */
-
         QueryWrapper<Asset> wrapper = new QueryWrapper<>();
-        wrapper.in("responsible_person_code", dto.getResponsiblePersonCodes());
-        List<Asset> assetList = assetMapper.selectList(wrapper);
-        if (!CollectionUtils.isEmpty(assetList)) {
-            task.setAssetNotCounted(assetList.size());
+        wrapper.setEntity(new Asset());
+
+        Long deptId =  Long.valueOf(task.getInventoryDept());
+        List<SysDept> sysDeptList = sysDeptService.selectDeptByParentId(deptId);
+        SysDept sysDept =new SysDept();
+        sysDept.setDeptId(deptId);
+        sysDeptList.add(sysDept);
+        List<Asset>  list =new ArrayList();
+        for (SysDept sd : sysDeptList) {
+            List<SysUser> sysUserList = sysUserService.selectUserByDeptId(sd.getDeptId());
+            for (SysUser s : sysUserList) {
+                wrapper.getEntity().setResponsiblePersonCode(s.getUserName());
+                List<Asset> assetList = assetMapper.selectList(wrapper);
+                if (assetList.size()>0){
+                    for (Asset asset : assetList) {
+                        list.add(asset);
+                    }
+                }
+            }
         }
-
         task.setCreateTime(new Date());
+        String str = DateUtils.dateTimeNow();
+        str+=RandomUtil.randomString(15);
+        task.setTaskCode(str);
+        String userName = SecurityUtils.getLoginUser().getUser().getUserName();
+        task.setCreateBy(userName);
+        if (task.getInventoryUserList()!=null){
+            task.setInventoryUsers(task.getInventoryUserList().toString());
+        }
+        assetInventoryTaskMapper.insert(task);
+        for (Asset asset : list) {
+            AssetProcess assetProcess = new AssetProcess();
+            assetProcess.setAssetCode(asset.getAssetCode());
+            assetProcess.setUserCode(userName);
+            assetProcess.setProcessType("1000");
+            assetProcess.setCreateTime(new Date());
+            assetProcessMapper.insert(assetProcess);
 
-        for (Asset asset : assetList) {
             AssetProcessCounting entity = new AssetProcessCounting();
             entity.setTaskCode(task.getTaskCode());
-            entity.setAssetCode(asset.getAssetCode());
+            entity.setAssetCode(asset.getCreateBy());
+            entity.setProcessId(assetProcess.getId());
             entity.setCreateTime(new Date());
             entity.setCountingStatus(AssetCountingStatus.NOT_COUNTED.getStatus());
             assetProcessCountingService.insertAssetProcessCounting(entity);

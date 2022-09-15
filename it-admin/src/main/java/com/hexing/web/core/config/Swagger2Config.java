1cbf07396a6f12b226fe1ae88f0ffc694a314d69
朱祥雨(80010641) <xiangyu.zhu@hxgroup.com>
2022-09-15 11:26:42 +0800

    knife集成

---
+++ it-admin/src/main/java/com/hexing/web/core/config/Swagger2Config.java
@@ -0,0 +1,46 @@
+package com.hexing.web.core.config;
+
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+//http://xxxx/doc.html#/
+@Configuration
+@EnableSwagger2WebMvc
+@EnableKnife4j
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                //是否开启 (true 开启  false隐藏。生产环境建议隐藏)
+                //.enable(false)
+                .select()
+                //扫描的路径包,设置basePackage会将包下的所有被@Api标记类的所有方法作为api
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                //指定路径处理PathSelectors.any()代表所有的路径
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //设置文档标题(API名称)
+                .title("项目接口API")
+                //文档描述
+                .description("接口说明")
+                //版本号
+                .version("1.0.0")
+                .build();
+    }
+}

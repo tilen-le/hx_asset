bb656b108d136cdb7af4ba7829531b7c3fc08469
80009313 <@80009313>
2022-09-21 17:15:58 +0800

    盘点任务-盘点时间显示时分秒

--- asset-ui/src/views/asset/task/detail.vue
+++ asset-ui/src/views/asset/task/detail.vue
@@ -57,7 +57,7 @@
       <el-table-column label="盘点人" align="center" prop="userNickName" />
       <el-table-column label="盘点时间" align="center" prop="countingTime" width="180">
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.countingTime, '{y}-{m}-{d}') }}</span>
+          <span>{{ parseTime(scope.row.countingTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
         </template>
       </el-table-column>
       <el-table-column label="异常信息" align="center" prop="comment" />

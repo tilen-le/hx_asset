ccecf97ea04e53b70cfb2be82e98f3b7f0f64a8c
邵天扬 <codeinpeace@163.com>
2022-09-26 16:31:48 +0800

    统计功能传参基础类修改

--- it-asset/src/main/java/com/hexing/asset/domain/vo/SimpleStatisticVO.java
+++ it-asset/src/main/java/com/hexing/asset/domain/vo/SimpleStatisticVO.java
@@ -1,9 +1,11 @@
 package com.hexing.asset.domain.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
+@AllArgsConstructor
 @Accessors(chain = true)
 public class SimpleStatisticVO {
     private String name;

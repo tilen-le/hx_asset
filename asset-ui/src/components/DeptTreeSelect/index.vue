de4eaf58f1d1a4e613085dd9a3eb1ad4f9c4f036
80009313 <@80009313>
2022-09-26 16:25:39 +0800

    盘点统计页面 部门树组件

---
+++ asset-ui/src/components/DeptTreeSelect/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <treeselect v-model="inValue" :options="deptOptions" :show-count="true" placeholder="请选择盘点组织"
+    @click.native="getTreeSelect" />
+</template>
+
+<script>
+import { treeselect } from '@/api/system/dept'
+import Treeselect from '@riophae/vue-treeselect'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+export default {
+  name: "deptTreeSelect",
+  props: {
+    value: {},
+  },
+  components: { Treeselect },
+  data() {
+    return {
+      inValue: this.value,
+      // 部门树选项
+      deptOptions: [],
+    }
+  },
+  watch: {
+    val() {
+      this.inValue = this.value
+    },
+    inValue() {
+      this.$emit('input', this.inValue)
+    }
+  },
+  methods: {
+    /** 查询部门下拉树结构 */
+    getTreeSelect() {
+      if (this.deptOptions.length == 0) {
+        treeselect().then(response => {
+          this.deptOptions = response.data;
+        });
+      }
+    },
+  }
+
+}
+</script>
+
+<style>
+
+</style>

95b7093db008fc14dda620968d261699c2e67605
朱祥雨(80010641) <xiangyu.zhu@hxgroup.com>
2022-10-09 14:12:21 +0800

    登录提示

--- asset-ui/src/utils/request.js
+++ asset-ui/src/utils/request.js
@@ -56,9 +56,9 @@ service.interceptors.response.use(res => {
     // 获取错误信息
     const msg = errorCode[code] || res.data.msg || errorCode['default']
     if (code === 401) {
-      MessageBox.confirm('登录状态已过期', 'Tip', {
-          confirmButtonText: 'ReLogin',
-          cancelButtonText: 'Cancel',
+      MessageBox.confirm('登录状态已过期', '提示', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
           type: 'warning'
         }
       ).then(() => {

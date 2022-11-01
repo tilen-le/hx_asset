999c821a23e056c552c229dc5583246d8515ef94
80009313 <@80009313>
2022-11-01 16:11:04 +0800

    流程字段配置表前台

---
+++ asset-ui/src/api/process/field.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询【请填写功能名称】列表
+export function listField(query) {
+  return request({
+    url: '/mature/field/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询【请填写功能名称】详细
+export function getField(id) {
+  return request({
+    url: '/mature/field/' + id,
+    method: 'get'
+  })
+}
+
+// 新增【请填写功能名称】
+export function addField(data) {
+  return request({
+    url: '/mature/field',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改【请填写功能名称】
+export function updateField(data) {
+  return request({
+    url: '/mature/field',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除【请填写功能名称】
+export function delField(id) {
+  return request({
+    url: '/mature/field/' + id,
+    method: 'delete'
+  })
+}
+
+// 导出【请填写功能名称】
+export function exportField(query) {
+  return request({
+    url: '/mature/field/export',
+    method: 'get',
+    params: query
+  })
+}

4749e70ab5b7d24165de9bc0537e79b1faa94b0d
80010712 <15122505230@163.com>
2023-02-09 14:26:15 +0800

    资产流程

--- it-asset/src/main/java/com/hexing/asset/domain/AssetManagementConfig.java
+++ it-asset/src/main/java/com/hexing/asset/domain/AssetManagementConfig.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hexing.common.annotation.Excel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -27,59 +28,72 @@ public class AssetManagementConfig
 
     /** 资产管理配置人员id */
     @TableId(type = IdType.AUTO)
+    @ApiModelProperty(value = "资产管理配置人员id")
     private Long id;
 
     /** 资产大类 */
     @Excel(name = "资产大类")
+    @ApiModelProperty(value = "资产大类")
     private String assetType;
 
     /** 资产中类 */
     @Excel(name = "资产中类")
+    @ApiModelProperty(value = "资产中类")
     private String assetCategory;
 
     /** 资产小类 */
     @Excel(name = "资产小类")
+    @ApiModelProperty(value = "资产小类")
     private String assetSubCategory;
 
-    /** 归属主体 */
-    @Excel(name = "归属主体")
+    /** 所属公司 */
+    @Excel(name = "所属公司")
+    @ApiModelProperty(value = "所属公司")
     private String company;
 
     /** 资产管理员 */
     @Excel(name = "资产管理员")
+    @ApiModelProperty(value = "资产管理员")
     private String assetManager;
 
     /** 账务管理员 */
     @Excel(name = "账务管理员")
+    @ApiModelProperty(value = "账务管理员")
     private String financialManager;
 
     /**
      * 创建时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "创建时间")
     private Date createTime;
 
     /** 发起人 */
     @Excel(name = "创建人")
+    @ApiModelProperty(value = "创建人")
     private String createBy;
 
     /**
      * 更新时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "更新时间")
     private Date updateTime;
 
     /** 修改人 */
     @Excel(name = "修改人")
+    @ApiModelProperty(value = "修改人")
     private String updateBy;
 
     /** 资产管理部门 */
     @Excel(name = "资产管理部门")
     @TableField(exist = false)
+    @ApiModelProperty(value = "资产管理部门")
     private String assetManageDept;
 
     /** 财务管理部门 */
     @Excel(name = "财务管理部门")
     @TableField(exist = false)
+    @ApiModelProperty(value = "财务管理部门")
     private String financialManageDept;
 }
--- it-asset/src/main/java/com/hexing/asset/domain/AssetProcess.java
+++ it-asset/src/main/java/com/hexing/asset/domain/AssetProcess.java
@@ -1,6 +1,7 @@
 package com.hexing.asset.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -16,70 +17,87 @@ public class AssetProcess {
     /**
      * 资产id
      */
+    @ApiModelProperty(value = "资产id")
     private String assetId;
     /**
      * 资产编码
      */
+    @ApiModelProperty(value = "资产编码")
     private String assetCode;
     /**
      * 资产名称
      */
+    @ApiModelProperty(value = "资产名称")
     private String assetName;
     /**
      * 规格型号
      */
+    @ApiModelProperty(value = "规格型号")
     private String standard;
     /**
      * 领用人工号
      */
+    @ApiModelProperty(value = "保管人工号")
     private String responsiblePersonCode;
     /**
      * 领用人名称
      */
+    @ApiModelProperty(value = "保管人名称")
     private String responsiblePersonName;
     /**
      * 领用人保管部门
      */
+    @ApiModelProperty(value = "保管部门")
     private String responsiblePersonDept;
     /**
      * 领用人岗位
      */
+    @ApiModelProperty(value = "保管人岗位")
     private String responsiblePersonJob;
     /**
      * 存放地点
      */
+    @ApiModelProperty(value = "存放地点")
     private String currentLocation;
     /**
      * 出厂编码
      */
+    @ApiModelProperty(value = "出厂编码")
     private String factoryNo;
     /**
-     * 出厂编码
+     * 资产分类
      */
+    @ApiModelProperty(value = "资产分类")
     private String assetCategory;
     /**
      * 成本中心
      */
+    @ApiModelProperty(value = "成本中心")
     private String costCenter;
     /**
      * 所属公司
      */
+    @ApiModelProperty(value = "所属公司")
     private String company;
     /**
      * 资产状态
      */
+    @ApiModelProperty(value = "资产状态")
     private String assetStatus;
     /**
      * 是否转固
      */
+    @ApiModelProperty(value = "是否转固")
     private Boolean fixed;
     /**
      * 采购单号
      */
+    @ApiModelProperty(value = "采购单号")
     private String purchaseOrderNo;
     /**
      * 转固验收日期
      */
+    @ApiModelProperty(value = "转固验收日期")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date fixedAcceptanceDate;
 

3932ea362e2ac38597f1cc25b8539b230f643e8f
80010712 <15122505230@163.com>
2023-02-09 14:31:40 +0800

    资产流程配置

---
+++ it-asset/src/main/java/com/hexing/asset/mapper/AssetProcessFieldMapper.java
@@ -0,0 +1,16 @@
+package com.hexing.asset.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hexing.asset.domain.AssetProcessField;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 流程字段表Mapper接口
+ *
+ * @author zxy
+ * @date 2022-11-01
+ */
+@Repository
+public interface AssetProcessFieldMapper extends BaseMapper<AssetProcessField> {
+
+}

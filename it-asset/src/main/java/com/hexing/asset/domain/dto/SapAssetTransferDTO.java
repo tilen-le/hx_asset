50548a915294e7a08e83ab41566036e14b3066d5
邵天扬 <codeinpeace@163.com>
2023-02-14 10:28:49 +0800

    资产转移DTO

---
+++ it-asset/src/main/java/com/hexing/asset/domain/dto/SapAssetTransferDTO.java
@@ -0,0 +1,46 @@
+package com.hexing.asset.domain.dto;
+
+import javafx.stage.Stage;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * 资产转移 DTO
+ *
+ * @author sty
+ * @date 2023-02-13
+ */
+@Data
+@EqualsAndHashCode
+@Accessors(chain = true)
+public class SapAssetTransferDTO {
+    /**
+     * 资产编码
+     */
+    private String assetCode;
+    /**
+     * 接收公司代码
+     */
+    private String BUKRS;
+    /**
+     * 接收人
+     */
+    private String Rname;
+    /**
+     * 接收人岗位
+     */
+    private String Post;
+    /**
+     * 成本中心编码
+     */
+    private String KOSTL;
+    /**
+     * 所在位置
+     */
+    private String Stage;
+    /**
+     * SAP资产编码
+     */
+    private String Anln1;
+}
